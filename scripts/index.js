2c08664a776edab71fb2f0ad6d02698678d9a0ba
Marina Fominykh <FominykhMM@yandex.ru>
2022-01-15 11:36:35 +0300

    устранить замечания 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-const initialCards = [{
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-
-];
-
 const editPopup = document.querySelector('.popup_type_edit');
 const addCardPopup = document.querySelector('.popup_type_add-card');
 const imagePopup = document.querySelector('.popup_type_activ-image');
@@ -48,35 +21,35 @@ const profileJob = document.querySelector('.profile__subtitle');
 const list = document.querySelector('.places__cards');
 const cardTemplate = document.querySelector('.card-template').content.querySelector('.places__card');
 
-function togglePopup(popup) {
-    popup.classList.toggle('popup_opened');
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
 }
 
-closeEditPopupButton.addEventListener('click', () =>
-    togglePopup(editPopup));
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+}
 
-addCardButton.addEventListener('click', () =>
-    togglePopup(addCardPopup));
-closeAddCardPopupButton.addEventListener('click', () =>
-    togglePopup(addCardPopup));
-closeImagePopupButton.addEventListener('click', () =>
-    togglePopup(imagePopup));
+closeEditPopupButton.addEventListener('click', () => closePopup(editPopup));
 
-function fullInput() {
+addCardButton.addEventListener('click', () => openPopup(addCardPopup));
+closeAddCardPopupButton.addEventListener('click', () => closePopup(addCardPopup));
+closeImagePopupButton.addEventListener('click', () => closePopup(imagePopup));
+
+function handleOpenProfileForm() {
     inputName.value = profileName.textContent;
     inputJob.value = profileJob.textContent;
-    togglePopup(editPopup);
+    openPopup(editPopup);
 }
-editProfileButton.addEventListener('click', fullInput);
+editProfileButton.addEventListener('click', handleOpenProfileForm);
 
-function formSubmitHandler(evt) {
+function formEditPopapSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
-    togglePopup(editPopup);
+    closePopup(editPopup);
 }
 
-editForm.addEventListener('submit', formSubmitHandler);
+editForm.addEventListener('submit', formEditPopapSubmitHandler);
 
 addCardForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -84,7 +57,7 @@ addCardForm.addEventListener('submit', (event) => {
         name: inputTitle.value,
         link: inputLink.value,
     });
-    togglePopup(addCardPopup)
+    closePopup(addCardPopup)
 });
 
 function getCard(item) {
@@ -93,8 +66,8 @@ function getCard(item) {
     const cardTitle = cardElement.querySelector('.places__name')
     const deleteButton = cardElement.querySelector('.places__delete-button')
     const likeButton = cardElement.querySelector('.places__like')
-    const imageActiv = imagePopup.querySelector('.popup__image-activ')
-    const imageRext = imagePopup.querySelector('.popup__image-text')
+    const imageActive = imagePopup.querySelector('.popup__image-activ')
+    const imageText = imagePopup.querySelector('.popup__image-text')
 
     cardTitle.textContent = item.name
     cardImage.src = item.link
@@ -109,9 +82,10 @@ function getCard(item) {
     })
 
     cardImage.addEventListener('click', () => {
-        togglePopup(imagePopup)
-        imageActiv.src = item.link
-        imageRext.textContent = item.name
+        openPopup(imagePopup)
+        imageActive.src = item.link
+        imageText.textContent = item.name
+        imageActive.alt = item.name
     })
 
     return cardElement
